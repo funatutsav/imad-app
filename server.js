fb126e242659cc2ce0e0300a5045c39d811de4de
funatutsav <funatutsav@gmail.com>
2017-08-03 21:12:37 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.get('/ui/style.css', function (req, res) {
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
-app.get('/ui/photo.jpg', function (req, res) {
+app.get('photo.jpg', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'photo.jpg'));
 });
 
